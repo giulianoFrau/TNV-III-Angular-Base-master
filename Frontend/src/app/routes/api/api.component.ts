4e552915b32043702653edcab1c4657f505c6383
Anna <annigru@hotmail.it>
2021-03-10 18:52:56 +0100

    implementazioni varie grafica

--- Frontend/src/app/routes/api/api.component.ts
+++ Frontend/src/app/routes/api/api.component.ts
@@ -12,33 +12,18 @@ export class ApiComponent implements OnInit {
 
   constructor(private apiService: ApiserviceService) { }
   dataArray: ApiCountryData[];
-  rischio: string;
   name: string;
-  code: any;
-
-
 
   ngOnInit(): void {
     this.apiService.getPaese().subscribe((response: ApiCountryList) => {
       this.dataArray = response.data;
-       this.checkRischio();
-      
     },
       err => console.log(err),
       () => console.log("done loading countries",)
     )
   }
   
-    checkRischio(){
-      for(let i=0;i<this.dataArray.length;i++){
-        if(this.dataArray[i].latest_data.deaths>1000){
-          this.rischio="alto";
-          
-        }else{
-          this.rischio="basso";
-        }
-      }
-    }
+
 
   Search() {
 
